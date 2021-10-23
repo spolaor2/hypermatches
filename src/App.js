9871169a2813c1b165bb930a9c023a18a1f409fb
Mateus A. Spolaor <mateus.spolaor2@gmail.com>
2021-10-23 16:09:32 -0300

    Com preto e branco

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import React from "react";
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+    this.handler = this.handler.bind(this)
+  }
+
   state = {
     data: [
       {
@@ -41,41 +49,59 @@ export default class App extends React.Component {
       {
         "name": "Vertigo",
         "photo": '/images/vertigo.png'
-      },
-
+      }
     ]
   }
 
   render() {
+    
     return (
       <View style={styles.container}>
         <FlatList
           style={{ flex: 1 }}
           data={this.state.data}
           renderItem={({ item }) => <Item item={item} />}
-          keyExtractor={item => item.email}
         />
       </View>
     );
   }
 }
 
-function Item({ item }) {
-  return (
-    <View style={styles.listItem}>
-      <ImageBackground source={{ uri: item.photo }} style={styles.image}>
-        <Text style={styles.paragraph}>
-          {item.name}
-        </Text>
-      </ImageBackground>
-      
-    </View>
-  );
+class Item extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedImage: false
+    }
+  }
+
+  
+
+  changeColor() {
+    this.setState({ selectedImage: !this.state.selectedImage })
+  }
+
+
+  render() {
+    return (
+      <View style={styles.listItem} >
+        <ImageBackground
+          source={{ uri: this.props.item.photo }}
+          style={[this.state.selectedImage ? styles.imageSelected : styles.imageNotSelected]}
+          onClick={this.changeColor.bind(this)} >
+          <Text style={styles.paragraph}>
+            {this.props.item.name}
+          </Text>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const win = Dimensions.get('window');
 
-const ratio = win.width/541;
+const ratio = win.width / 541;
 
 const styles = StyleSheet.create({
   container: {
@@ -85,12 +111,20 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     justifyContent: 'center'
   },
-  image: {
+  imageSelected: {
     alignItems: 'center',
-    justifyContent:'center',
-    width: win.width/1.5,
+    justifyContent: 'center',
+    width: win.width / 1.5,
     height: 30 * ratio,
-    resizeMode: "cover"
+    resizeMode: 'cover',
+    filter: "grayscale(100%)"
+  },
+  imageNotSelected: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: win.width / 1.5,
+    height: 30 * ratio,
+    resizeMode: 'cover',
   },
   listItem: {
     margin: 10,
@@ -101,7 +135,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     borderRadius: 5,
     alignItems: 'center',
-    justifyContent:'center'
+    justifyContent: 'center'
   },
   paragraph: {
     textAlign: 'center',
